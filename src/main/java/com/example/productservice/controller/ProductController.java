9c7a8625288052e657060618122b5cbdb305da8f
Arsen Keshishyan <a01576524@unet.univie.ac.at>
2023-04-02 18:29:11 +0200

    productMS

--- src/main/java/com/example/productservice/controller/ProductController.java
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.example.productservice.controller;
 
 
+import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.dto.ProductResponse;
+import com.example.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -20,8 +25,8 @@ public class ProductController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponse> getAllProdcuts() {
-        return productService.getAllProdcuts();
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 
 }
